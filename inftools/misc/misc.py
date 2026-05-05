88a0cd96fdb34bd0f9b350429c89182a55127fb7
dz24 <rubidxx.dz@gmail.com>
2026-05-05 10:51:50 +0900

    update

--- inftools/misc/misc.py
+++ inftools/misc/misc.py
@@ -1,4 +1,13 @@
 def assign_code(log):
+    """
+    inp: sim.log
+
+    Assign unique path identifiers "code" for pns, that are not
+    necessarily unique.
+
+    Additionally, returns the pns for each infinit round and
+    the total amount of shooting attempts performed.
+    """
     paths = {}
     codes = {}
     inits = []
@@ -21,7 +30,8 @@ def assign_code(log):
                 if init_b:
                     inits0.append(pn)
                 if pn not in paths:
-                    code = f"{rip[-1]}_{rip[-2]}_{rip[-3]}"
+                    # sometimes there is rounding in the OP
+                    code = f"{rip[-1]}_{rip[-2][:-1]}_{rip[-3][:-1]}"
                     paths[pn] = code
                     shootings.append(shooting)
                     if code not in codes:
@@ -37,6 +47,9 @@ def assign_code(log):
 
 
 def add_move(replica, ens_arr, old, new, ens_num):
+    """
+    Add the new path pn and ensemble to the replica pn and ens list.
+    """
     replica_new = []
     ens_new = []
 
@@ -52,16 +65,24 @@ def add_move(replica, ens_arr, old, new, ens_num):
     return replica_new, ens_new
 
 
-def read_log(log, pn):
-    # what we want:
-    # p0 -> p10 -> p16 -> p20 -> p23 -> p31 -> p34 -> p50
-    # e0 ->  e1 ->  e0 ->  e1 ->  e1 ->  e0 ->  e0 ->  e1
+def step_flow(log, inits):
+    """
+    Return the replica flows individual infinit steps (infretis run)
+    and their associated ensmbes
 
-    paths, codes, inits, shootings = assign_code(log)
-
+    e.g. for 1 infinit step,
+    p0
+    p1>p4
+    p2>p5
+    p3
+    meaning only p1 and p2 had successful shooting moves and so on
+    for other steps.
+    """
     istep = -1
     replicas = []
     ensembles = []
+    imcsteps = []
+    imcsteps0 = 0
     with open(log) as read:
         for line in read:
             rip = line.rstrip().split()
@@ -72,12 +93,15 @@ def read_log(log, pn):
                 if istep > -1:
                     replicas.append(replicas0)
                     ensembles.append(ens0)
+                    imcsteps.append(imcsteps0)
+                    imcsteps0 = 0
                 istep += 1
                 replicas0 = [f"{i}" for i in inits[istep]]
                 ens0 = [str(i) for i in range(len(replicas0))]
 
             if "shooted" not in line:
                 continue
+            imcsteps0 += 1
 
             # zero swap
             if "shooted" in line and len(rip) == 15:
@@ -97,7 +121,24 @@ def read_log(log, pn):
                 replicas0, ens0 = add_move(replicas0, ens0, pno, pnn, ens)
     replicas.append(replicas0)
     ensembles.append(ens0)
+    imcsteps.append(imcsteps0)
+    return replicas, ensembles, imcsteps
 
+
+def read_log(log, pn):
+    """
+    return the whole replica flow for a pn throughout all
+    infinit steps
+    """
+    # what we want:
+    # p0 -> p10 -> p16 -> p20 -> p23 -> p31 -> p34 -> p50
+    # e0 ->  e1 ->  e0 ->  e1 ->  e1 ->  e0 ->  e0 ->  e1
+
+    # get pn, pn id, init rounds and shootings
+    paths, codes, inits, shootings = assign_code(log)
+    replicas, ensembles, imcsteps = step_flow(log, inits)
+
+    # connect replica flow for a pn code between individiual infinit steps
     follow_pn = []
     follow_en = []
     follow_size = []
@@ -113,13 +154,33 @@ def read_log(log, pn):
                 break
         follow_size.append((len(replica), len(rep_codesl)))
 
+    # connect all pns, ens and codes together
     pns = ("+".join(follow_pn[::-1])).split("+")
     ens = ("+".join(follow_en[::-1])).split("+")
+    cds = [paths[int(pn)] for pn in pns]
 
-    paths = list(paths.keys())
-    shootings0 = {pn: sht for pn, sht in zip(paths, shootings)}
+    # here multiple pns may refer to the same path (code).
+    # we select the unique only here
+    pns_uniq = []
+    ens_uniq = []
+    codes0 = []
+    for ens0, pn in zip(ens, pns):
+        code0 = paths[int(pn)]
+        if code0 not in codes0:
+            pns_uniq.append(pn)
+            ens_uniq.append(ens0)
+            codes0.append(code0)
+
+    # connect shooting with pn
+    pathsl = list(paths.keys())
+    shootings0 = {pn: sht for pn, sht in zip(pathsl, shootings)}
     shootings1 = []
     for pn in pns:
         shootings1.append(shootings0[int(pn)])
 
-    return ens, pns, follow_size[::-1], shootings1
+    # connect shooting with pn_unique
+    shootings2 = []
+    for pn in pns_uniq:
+        shootings2.append(shootings0[int(pn)])
+
+    return ens, pns, follow_size[::-1], shootings1, pns_uniq, ens_uniq, shootings2, imcsteps
--- inftools/tistools/calc_iflow.py
+++ inftools/tistools/calc_iflow.py
@@ -14,7 +14,11 @@ def calc_iflow(
 
     Returns a flow_map dictionary.
     """
+    import numpy as np
     import matplotlib.pyplot as plt
+
+    # import scienceplots
+    # plt.style.use('science')
 
     from inftools.misc.misc import read_log
     from inftools.tistools.max_op import COLS
@@ -22,18 +26,22 @@ def calc_iflow(
 
     for idx0, rep in enumerate([int(i) for i in plot.split(" ")]):
 
-        ens, pns, follow_size, shootings = read_log("sim.log", rep)
-        print("ens:", " ".join(ens))
-        print("pns:", " ".join(pns))
+        ens, pns, follow_size, shootings, pns_uniq, ens_uniq, shootings2, imcsteps = read_log(log, rep)
+        print("ens:", " ".join(pns_uniq))
+        print("pns:", " ".join(ens_uniq))
 
-        plt.plot(shootings, [int(i) for i in ens], color=COLS[idx0])
-        plt.scatter(shootings, [int(i) for i in ens], color=COLS[idx0])
+        plt.plot(shootings2, [int(i) for i in ens_uniq], color=COLS[idx0%len(COLS)], zorder=10)
+        plt.scatter(shootings2, [int(i) for i in ens_uniq], color=COLS[idx0%len(COLS)], zorder=10, s=15, edgecolor="k")
         if idx0 == 0:
-            acc = 50
-            for idx, fs in enumerate(follow_size):
-                plt.plot([acc*idx, acc*(idx+1)], [fs[0]-1]*2, color="k")
-            # acc += fs[1]
-            # acc += 50
+            accum = np.cumsum([0] + imcsteps)
+            for idx, (istep, fs) in enumerate(zip(imcsteps, follow_size)):
+                plt.plot([accum[idx], accum[idx+1]], [fs[0]]*2, color="k", ls="--")
+                if idx < len(imcsteps)-1:
+                    dfs = follow_size[idx+1][0]-follow_size[idx][0]
+                    plt.plot([accum[idx+1]]*2, [0, follow_size[idx][0] + dfs], color="k", ls="--", alpha=0.1)
+                    if dfs == 0:
+                        continue
+                    plt.plot([accum[idx+1]]*2, [follow_size[idx][0], follow_size[idx][0] + dfs], color="k", ls="--")
 
 
     plt.xlabel("MC Moves")
